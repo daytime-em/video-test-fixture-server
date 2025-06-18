38f96eeb971e4e5d8c989dc6eaafb19c12cd4fff
Emily Dixon <edixon@mux.com>
2025-06-18 14:44:55 -0500

    fewer text files

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,11 @@ const server = new FixtureServer({ port: 3000 });
   await server.listen();
   console.log('Fixture server running at http://localhost:3000');
 
-  // Usage examples:
+  // Just the text files
   server.setFixtureFileResponseTime('file1.txt', 2000);
-  server.setFixtureFileResponseBitrate('file4.txt', 256 * 1024);
-  server.setFixtureFileHeaders('file4.txt', {
+  server.setFixtureFileResponseBitrate('file2.txt', 256 * 1024);
+  server.setFixtureFileHeaders('file2.txt', {
     'X-CDN': "edgemv",
   });
-  server.setFixtureFileHeaders('file1.txt', { 'X-Demo-Header': 'example', 'Content-Language': 'en' });
-  server.setFixtureFileHeaders('file2.txt', { 'Cache-Control': 'no-store' });
 
 })();
