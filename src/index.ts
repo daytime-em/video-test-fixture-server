3ef8f1f9640ab5965bc3befd2a765924b65d19d8
Emily Dixon <edixon@mux.com>
2025-06-18 18:06:48 -0500

    ok

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,24 @@ import { FixtureServer } from './fixture-server';
 import { types, parse } from 'hls-parser';
 // import fs from 'fs';
 import { promises as fs } from 'fs';
+import { MasterPlaylist } from 'hls-parser/types';
+import path from 'path';
 
 const port = 3000;
 const server = new FixtureServer({ port: port});
+const FILES_DIR = path.join(__dirname, 'files');
 
 (async () => {
   await server.listen();
   console.log(`Fixture server running at http://localhost:${port}`);
 
   // Set up a fake CDN Change
-  const mvp = parse((await fs.readFile('mux-promo/stream.m3u8')).toString('utf8'));
+  const streamName = "mux-promo"
+  const streamPath = `${FILES_DIR}/${streamName}`
+  const mvp = parse((await fs.readFile(`${streamPath}/stream.m3u8`)).toString('utf8')) as MasterPlaylist;
+  mvp.variants.forEach(async variant => {
+    console.log(`variant url is ${variant.uri}`);
+  });
   
 
   // Just the text files
